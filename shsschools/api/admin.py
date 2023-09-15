10aea61b98ad50138e96cc0004b0720efc201768
LawrenceDarko <darkolawrence@gmail.com>
2023-09-15 12:18:08 +0000

    admin changes

--- shsschools/api/admin.py
+++ shsschools/api/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import School
 
 # Register your models here.
-admin.site.register(School)
+# admin.site.register(School)
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'district', 'location')
+    search_fields = ('name', 'district', 'location', 'region')
